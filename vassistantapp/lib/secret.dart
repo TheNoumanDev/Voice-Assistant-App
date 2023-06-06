9a86786aa699e378128b00917c38a93172716e4b
TheNoumanDev <m_nouman@hotmail.com>
2023-06-06 23:33:02 +0500

    Removed Unused Files

--- vassistantapp/lib/secret.dart
+++ vassistantapp/lib/secret.dart
@@ -1,2 +1,2 @@
 const String OpenAiKey = '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';
-const String nOpenAiKey = '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';
+const String nOpenAiKey = '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';
